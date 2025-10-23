e3e84b4e1a207b17e620da4315ce9682ad4e021a
Shamil Radzhabov <shmiil@bk.ru>
2025-10-23 15:52:57 +0300

    added function buttons

---
+++ main.js
@@ -0,0 +1,40 @@
+const productCards = document.querySelectorAll('.product-card');
+const changeColorAllCardsButton = document.querySelector('#change-color-all-cards');
+const greenColorHash = '#00FF00';
+const bluColorHash = '#0000FF';
+
+changeColorAllCardButton.addEventListener('click',() => {
+  productCards.forEach((card) => card.style.backgroundColor = greenColorHash ) 
+})
+
+const firstProductCard = document.querySelector('.product-card');
+const chageColorFirstCardButton = document.querySelector('#change-color-first-card');
+
+
+chageColorFirstCardButton.addEventListener('click',() => {
+  firstProductCard.style.backgroundColor = bluColorHash
+})
+
+const openGoogleButton = document.querySelector('#open-google');
+openGoogleButton.addEventListener('click', openGoogle);
+
+function openGoogle() {
+  const answer = confirm('Вы действительно хотите открыть Google?');
+
+  if (answer === true) 
+    window.open('https://google.com');
+}
+
+const changingButtonColor = document.querySelector('.bg-darkmagenta');
+
+changingButtonColor.addEventListener('click', () => {
+  changingButtonColor.classList.toggle('bg-coral');
+}) 
+
+const title = document.querySelector('.main-title');
+const headerContent = title.textContent;
+
+title.addEventListener('mouseover', () => {
+  console.log(headerContent);
+})
+  
