3c9a110ad0af55c7d55771e3e31deb0cc9d23c14
Shamil Radzhabov <shmiil@bk.ru>
2025-10-29 10:47:32 +0300

    fixed new errors

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
+
+//Смена цвета всех карточек//
 const productCards = document.querySelectorAll('.product-card');
 const changeColorAllCardsButton = document.querySelector('#change-color-all-cards');
 const greenColorHash = '#00FF00';
-const bluColorHash = '#0000FF';
 
 changeColorAllCardsButton.addEventListener('click', () => {
   productCards.forEach((card) => card.style.backgroundColor = greenColorHash ) 
 })
 
+//Смена цвета первой картоочки//
 const firstProductCard = document.querySelector('.product-card');
 const chageColorFirstCardButton = document.querySelector('#change-color-first-card');
-
+const bluColorHash = '#0000FF';
 
 chageColorFirstCardButton.addEventListener('click', () => {
   firstProductCard.style.backgroundColor = bluColorHash
 })
 
+//Открыть Google//
 const openGoogleButton = document.querySelector('#open-google');
 openGoogleButton.addEventListener('click', openGoogle);
 
@@ -25,12 +28,14 @@ function openGoogle() {
     window.open('https://google.com');
 }
 
+//переключение цвета кнопки принажатии//
 const changingButtonColor = document.querySelector('.bg-aqua');
 
 changingButtonColor.addEventListener('click', () => {
   changingButtonColor.classList.toggle('bg-blue');
 }) 
 
+//Выведение в консоль принаведении на текст//
 const title = document.querySelector('.main-title');
 const headerContent = title.textContent;
 
