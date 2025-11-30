2a5d00e7ed98a1ce1a3a184f9c016fd661865ac5
Shamil Radzhabov <shmiil@bk.ru>
2025-11-30 13:41:58 +0300

    fixed a lot of bugs

--- homework-8.js
+++ homework-8.js
@@ -6,19 +6,19 @@ const productCardsList = document.querySelector('.product-wrapper');
 const productCardTemplate = document.querySelector('#product-card-template');
 productCards.forEach(card => {
   const productCardClone = productCardTemplate.content.cloneNode(true);
-  productCardClone.querySelector('#img').src = card.url;
-  productCardClone.querySelector('#img').alt = card.alt;
-  productCardClone.querySelector('#title').innerHTML = card.productTitle;
-  productCardClone.querySelector('#description').textContent = card.description;
+  productCardClone.querySelector('.product-img').src = `/images/${card.img}.png`
+  productCardClone.querySelector('.product-category').textContent = card.category
+  productCardClone.querySelector('.prouct-title').innerHTML = card.title;
+  productCardClone.querySelector('.product-description').textContent = card.description;
   productCardClone.querySelector('.product-compound-iteam').innerHTML = card.compound.map(item => `<li>${item}</li>`).join("");
-  productCardClone.querySelector('#price').innerHTML = card.price + '&#8381'
+  productCardClone.querySelector('.product-price').innerHTML = card.price + '&#8381'
   productCardsList.appendChild(productCardClone);
 });
 
 // №2 Используя метод .reduce(), получить строку, которая состоит из названий продуктовых карточек, разделенных точкой с запятой
 
 const getProductCardsTitle = productCards.reduce((acc, title) => {
-  acc.push(title.productTitle);
+  acc.push(title.title);
   return acc
 }, []);
 
@@ -27,24 +27,24 @@ console.log(productCardsTitleStrings);
 
 // №3 Используя метод .reduce(), получить массив объектов, где ключем является название продукта, а значением - его описание
 
-const ProductCardArray = productCards.reduce((acc, card) => {
+const productCardArray = productCards.reduce((acc, card) => {
     acc.push({[card.productTitle]: card.description});
     return acc;
 }, []);
 
-console.log(ProductCardArray);
+console.log(productCardArray);
 
 // №4 Реализовать функцию, которая при старте нашей страницы выводит сообщение с текстом.
 
+
 const showCards = () => {
-  const question = prompt("Сколько карточек отобразить? От 1 до 5");
-  const numbers = Number(question);
+  const count = prompt("Сколько карточек отобразить? От 1 до 5");
+  const numbers = Number(count);
   if (numbers > 0 && numbers <= productCards.length) {
     outputProductCards(productCards.slice(0, numbers));
   } else {
     alert("Введите число от 1 до 5");
-  }
-}
+  };
+};
 
 showCards()
-
